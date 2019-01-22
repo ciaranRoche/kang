e84b429226558ae52b21a6d6bf4c6889a59f25b5
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-01-22 16:29:30 +0000

    project skeleton

---
+++ app/src/main/java/org/ciaranroche/kang/ui/start/StartViewModel.kt
@@ -0,0 +1,15 @@
+package org.ciaranroche.kang.ui.start
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class StartViewModel : ViewModel() {
+    private val _data = MutableLiveData<String>()
+    val data : LiveData<String>
+        get() = _data
+
+    init {
+        _data.value = "Hello, Jetpack!"
+    }
+}
