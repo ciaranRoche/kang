afc8d783b4b8271212833ba1ee43b000a6b2bca9
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-01-26 19:53:03 +0000

    add nav component, dummy pages and initial deep link

---
+++ app/src/main/java/org/ciaranroche/kang/FlowStepFragment.kt
@@ -0,0 +1,35 @@
+package org.ciaranroche.kang
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+
+class FlowStepFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+
+        val safeArgs = FlowStepFragmentArgs.fromBundle(arguments!!)
+        val flowStepNumber = safeArgs.flowStepNumber
+
+        return when (flowStepNumber) {
+            2 -> inflater.inflate(R.layout.flow_step_two_fragment, container, false)
+            else -> inflater.inflate(R.layout.flow_step_one_fragment, container, false)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.next_button).setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.next_action)
+        )
+    }
+}
