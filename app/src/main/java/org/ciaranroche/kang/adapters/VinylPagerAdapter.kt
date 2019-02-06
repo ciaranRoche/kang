fc557b28d4868256529eebd01a69f9a50d651e45
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-02-06 12:41:36 +0000

    add initial home pager

---
+++ app/src/main/java/org/ciaranroche/kang/adapters/VinylPagerAdapter.kt
@@ -0,0 +1,29 @@
+package org.ciaranroche.kang.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import org.ciaranroche.kang.fragments.VinylProfileFragment
+import org.ciaranroche.kang.models.VinylModel
+
+interface VinylListener {
+    fun onVinylClick(vinyl: VinylModel)
+}
+
+class VinylPagerAdapter(fragmentManager: FragmentManager, private val vinyls: ArrayList<VinylModel>) : FragmentStatePagerAdapter(fragmentManager) {
+
+    private val MAX_VALUE = 200
+
+    override fun getItem(position: Int): Fragment {
+        return VinylProfileFragment.newInstance(vinyls[position % vinyls.size])
+    }
+
+    override fun getCount(): Int {
+        return vinyls.size * MAX_VALUE
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return vinyls[position % vinyls.size].name
+    }
+
+}
--- app/src/main/java/org/ciaranroche/kang/fragments/HomeFragment.kt
+++ app/src/main/java/org/ciaranroche/kang/fragments/HomeFragment.kt
@@ -2,41 +2,66 @@ package org.ciaranroche.kang.fragments
 
 import android.os.Bundle
 import android.view.*
-import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
+import androidx.viewpager.widget.ViewPager
+import com.nshmura.recyclertablayout.RecyclerTabLayout
 import org.ciaranroche.kang.R
+import org.ciaranroche.kang.adapters.VinylPagerAdapter
+import org.ciaranroche.kang.models.VinylJSONStore
+import org.ciaranroche.kang.models.VinylModel
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(){
+    lateinit var vinyls: MutableList<VinylModel>
+    lateinit var viewPager: ViewPager
+    lateinit var pagerAdapter: VinylPagerAdapter
+    lateinit var data: VinylJSONStore
+    private lateinit var recyclerTabLayout: RecyclerTabLayout
+
+
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.home_fragment, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_vinyl_profile, container, false)
+
+        data = VinylJSONStore(this.context!!.applicationContext)
+        data.seed()
+        vinyls = data.findAll()
+
+        viewPager = view.findViewById(R.id.viewPager)
+        pagerAdapter = VinylPagerAdapter(this.fragmentManager!!, vinyls as ArrayList<VinylModel>)
+
+        viewPager.adapter = pagerAdapter
+        viewPager.currentItem = pagerAdapter.count / 2
+        recyclerTabLayout = view.findViewById(R.id.recyclerTabLayout)
+        recyclerTabLayout.setUpWithViewPager(viewPager)
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = view.findViewById<Button>(R.id.btn_add_vinyl)
-        button?.setOnClickListener {
-            findNavController().navigate(R.id.add_vinyl)
-        }
+//        val button = view.findViewById<Button>(R.id.btn_add_vinyl)
+//        button?.setOnClickListener {
+//            findNavController().navigate(R.id.add_vinyl)
+//        }
 
-        val options = navOptions {
-            anim {
-                enter = R.anim.slide_in_right
-                exit = R.anim.slide_out_left
-                popEnter = R.anim.slide_in_left
-                popExit = R.anim.slide_out_right
-            }
-        }
-        view.findViewById<Button>(R.id.btn_add_vinyl)?.setOnClickListener {
-            findNavController().navigate(R.id.add_vinyl, null, options)
-        }
+//        val options = navOptions {
+//            anim {
+//                enter = R.anim.slide_in_right
+//                exit = R.anim.slide_out_left
+//                popEnter = R.anim.slide_in_left
+//                popExit = R.anim.slide_out_right
+//            }
+//        }
+//        view.findViewById<Button>(R.id.btn_add_vinyl)?.setOnClickListener {
+//            findNavController().navigate(R.id.add_vinyl, null, options)
+//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
