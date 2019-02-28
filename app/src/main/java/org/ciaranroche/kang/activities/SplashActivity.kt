1903c0793cb8c66de27c935eb06de26e17334b66
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-02-28 09:24:20 +0000

    update splash screen

---
+++ app/src/main/java/org/ciaranroche/kang/activities/SplashActivity.kt
@@ -0,0 +1,18 @@
+package org.ciaranroche.kang.activities
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import org.ciaranroche.kang.R
+import org.jetbrains.anko.intentFor
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivityForResult(intentFor<MainActivity>(), 0)
+        }, 2500)
+    }
+}
