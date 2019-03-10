4c39635a3bfe22ee903db447a8ab59afff6d3869
ciaranRoche <ciaranroche@hotmail.co.uk>
2019-03-10 09:50:05 +0000

    add delete user confirm

--- app/src/main/java/org/ciaranroche/kang/fragments/main/UserProfileFragment.kt
+++ app/src/main/java/org/ciaranroche/kang/fragments/main/UserProfileFragment.kt
@@ -9,13 +9,17 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.squareup.picasso.Picasso
 import org.ciaranroche.kang.R
+import org.ciaranroche.kang.activities.StartUpActivity
 import org.ciaranroche.kang.main.MainApp
 import org.ciaranroche.kang.models.user.UserModel
+import org.jetbrains.anko.support.v4.intentFor
+import org.jetbrains.anko.support.v4.toast
 import java.util.ArrayList
 
 class UserProfileFragment : Fragment() {
@@ -34,7 +38,8 @@ class UserProfileFragment : Fragment() {
         val userBio = view.findViewById<TextView>(R.id.userBio)
         val userImage = view.findViewById<ImageView>(R.id.imageView)
         val settingsFab = view.findViewById<FloatingActionButton>(R.id.settings_fab)
-        var collectionBtn = view.findViewById<Button>(R.id.collection_button)
+        val collectionBtn = view.findViewById<Button>(R.id.collection_button)
+        val deleteBtn = view.findViewById<Button>(R.id.deleteUserButton)
 
         userName.text = user.username
         userBio.text = user.bio
@@ -56,6 +61,40 @@ class UserProfileFragment : Fragment() {
             view.findNavController().navigate(R.id.action_userProfileFragment_to_vinylProfileFragment, bundle)
         }
 
+        deleteBtn.setOnClickListener {
+            confirmDelete(user)
+        }
+
         return view
+    }
+
+    fun confirmDelete(user: UserModel) {
+        val alert = AlertDialog.Builder(context!!)
+
+        with(alert) {
+            setTitle("Confirm Account Deletion")
+
+            setMessage("Are you sure you want to delete your account?")
+
+            setPositiveButton("Delete") {
+                    dialog, _ ->
+                dialog.dismiss()
+                deleteUser(user)
+            }
+
+            setNegativeButton("Cancel") {
+                    dialog, _ ->
+                dialog.dismiss()
+            }
+        }
+
+        val dialog = alert.create()
+        dialog.show()
+    }
+
+    fun deleteUser(user: UserModel) {
+        app.users.delete(user)
+        startActivityForResult(intentFor<StartUpActivity>(), 0)
+        toast("${user.username} Deleted")
     }
 }
